bde0f237acf2524fe236c5bd48dd739e4136922b
Ludwig Sickert <ludwigsickert@gmail.com>
2022-10-10 20:34:09 +0200

    rewrote file download for multiprocessing

--- data/utils.py
+++ data/utils.py
@@ -2,60 +2,73 @@ import io
 import shutil
 import requests
 import bz2
-from tqdm.auto import tqdm
+import multiprocessing as mp
+from tqdm import tqdm
 from pathlib import Path
 from functools import partial
 
 DATA_FOLDER = Path(__file__).parent.resolve()
 
 
-def get_data(force_redownload=False):
+def get_data(urllist: list, force_redownload: bool = False) -> list:
+    mp.freeze_support()
 
-    if not Path(DATA_FOLDER / "infobox-properties_lang=de.ttl.bz2").exists() or force_redownload:
-        fname = download_file(
-            "https://databus.dbpedia.org/dbpedia/generic/infobox-properties/2022.03.01/infobox-properties_lang=de.ttl.bz2")
-        extract_file(fname)
+    arglist = []
 
-    if not Path(DATA_FOLDER / "infobox-properties_lang=en.ttl.bz2").exists() or force_redownload:
-        fname = download_file(
-            "https://databus.dbpedia.org/dbpedia/generic/infobox-properties/2022.03.01/infobox-properties_lang=en.ttl.bz2")
-        extract_file(fname)
+    for idx, url in enumerate(urllist):
+        args = (url, force_redownload,idx)
+        arglist.append(args)
 
-    if not Path(DATA_FOLDER / "infobox-properties_lang=nl.ttl.bz2").exists() or force_redownload:
-        fname = download_file(
-            "https://databus.dbpedia.org/dbpedia/generic/infobox-properties/2022.03.01/infobox-properties_lang=nl.ttl.bz2")
-        extract_file(fname)
-    return
+    cpus = mp.cpu_count()
+    pool_size = min(len(urllist), cpus)
+
+    pool = mp.Pool(processes=pool_size, initializer=tqdm.set_lock,
+                   initargs=(mp.RLock(),))
+
+    filenames = pool.starmap(_get_files, arglist)
+
+    return filenames
 
 
-def download_file(url):
-    filename = url.split('/')[-1]
+def _get_files(url: str, force_redownload: bool = False, pid = None) -> str:
+
+    fname = url.split("/")[-1]
+
+    if not Path(DATA_FOLDER / fname).exists() or force_redownload:
+        _download_file(url, fname, pid)
+        if fname.split(".")[-1] == "bz2":
+            fname = _extract_file(fname, pid)
+
+    return fname
+
+
+def _download_file(url: str, filename: str, pid) -> None:
 
     with requests.get(url, stream=True) as res:
         if res.status_code != 200:
             res.raise_for_status()
             raise RuntimeError(f"{url} returned {res.status_code} status")
 
-        size = int(res.headers.get('Content-Length', 0))
+        size = int(res.headers.get("Content-Length", 0))
 
         res.raw.read = partial(res.raw.read, decode_content=True)
 
         desc = f"downloading {filename}"
-        with tqdm.wrapattr(res.raw, "read", total=size, desc=desc) as raw_res:
-            with open(DATA_FOLDER / filename, 'wb') as file:
+        with tqdm.wrapattr(res.raw, "read", total=size, desc=desc, position=pid) as raw_res:
+            with open(DATA_FOLDER / filename, "wb") as file:
                 shutil.copyfileobj(raw_res, file)
 
-        return filename
 
-
-def extract_file(file):
+def _extract_file(file: str, pid) -> str:
     fname = file[:-4]
     desc = f"extracting {file}"
     compfile = bz2.open(DATA_FOLDER / file)
     size = _get_file_size(compfile)
-    with tqdm.wrapattr(compfile, "read", total=size, desc=desc) as comp:
+    with tqdm.wrapattr(compfile, "read", total=size, desc=desc, position=pid) as comp:
         with open(DATA_FOLDER / fname, "wb") as out:
             shutil.copyfileobj(comp, out)
+
+    return fname
 
 
 def _get_file_size(f):
--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import data.utils
 
+LANG_FILES = [
+    "https://databus.dbpedia.org/dbpedia/generic/infobox-properties/2022.03.01/infobox-properties_lang=de.ttl.bz2",
+    "https://databus.dbpedia.org/dbpedia/generic/infobox-properties/2022.03.01/infobox-properties_lang=en.ttl.bz2",
+    "https://databus.dbpedia.org/dbpedia/generic/infobox-properties/2022.03.01/infobox-properties_lang=nl.ttl.bz2"
+]
+
 if __name__ == "__main__":
-    data.utils.get_data()
+
+    filenames = data.utils.get_data(LANG_FILES)
