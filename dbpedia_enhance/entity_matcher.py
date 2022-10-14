49cb9a36a5932aeca4de040923dcdc974eec029e
jan-harms <90192648+jan-harms@users.noreply.github.com>
2022-10-14 14:42:45 +0200

    added entity_matcher.py to find properties with matching values

---
+++ dbpedia_enhance/entity_matcher.py
@@ -0,0 +1,43 @@
+import os
+import os.path
+import csv
+
+def find_entity_match(property, src_lang, trg_lang):
+    """ find an entity with a given property in one language that also exists in another language."""
+    for filename in os.listdir(src_lang):
+        with open('{}/{}'.format(src_lang, filename), encoding='utf-8') as f:
+            srcreader = csv.reader(f)
+            for row in srcreader[1:]:
+                if row[0] == property:
+                    if os.path.isfile('{}/{}'.format(trg_lang, filename)):
+                        print('Match:', property, filename)
+
+                    """the following code is a start at opening the matching target file and looking for the property there."""
+                    #try:
+                    #    with open('{}/{}.csv'.format(trg_lang, filename), encoding='utf-8') as g:
+                    #        trgreader = csv.reader(g)
+                    #        for row in trgreader:
+                    #            if row[0] == property:
+                    #                prop_match = True
+
+def find_matching_prop(entity, src_lang, trg_lang):
+    """ for a given entity, find properties with matching values in both languages."""
+    with open('data/{}/{}.csv'.format(src_lang, entity), encoding='utf-8') as f:
+        try:
+            with open('data/{}/{}.csv'.format(trg_lang, entity), encoding='utf-8') as g:
+                print('step2')
+                srcreader = csv.reader(f)
+                trgreader = csv.reader(g)
+                prop_matches = []
+                for src_row in srcreader:
+                    print(src_row)
+                    for trg_row in trgreader:
+                        #print('comparing row')
+                        if src_row[1] == trg_row[1]:
+                            prop_matches.append([src_row[0], trg_row[0], src_row[1]])
+                print(entity, '\n', prop_matches)
+        except:
+            pass
+
+#find_entity_match('website', 'en', 'nl')
+find_matching_prop('!!!', 'en', 'nl')
