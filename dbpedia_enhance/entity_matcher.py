eb264ff6e5fa2d09458cb06e470978adca777993
lukeluna <luke@lukeluna.nl>
2022-10-14 15:16:36 +0200

    bug fixed

--- dbpedia_enhance/entity_matcher.py
+++ dbpedia_enhance/entity_matcher.py
@@ -21,23 +21,24 @@ def find_entity_match(property, src_lang, trg_lang):
                     #                prop_match = True
 
 def find_matching_prop(entity, src_lang, trg_lang):
-    """ for a given entity, find properties with matching values in both languages."""
-    with open('data/{}/{}.csv'.format(src_lang, entity), encoding='utf-8') as f:
-        try:
-            with open('data/{}/{}.csv'.format(trg_lang, entity), encoding='utf-8') as g:
-                print('step2')
-                srcreader = csv.reader(f)
-                trgreader = csv.reader(g)
-                prop_matches = []
-                for src_row in srcreader:
-                    print(src_row)
-                    for trg_row in trgreader:
-                        #print('comparing row')
-                        if src_row[1] == trg_row[1]:
-                            prop_matches.append([src_row[0], trg_row[0], src_row[1]])
-                print(entity, '\n', prop_matches)
-        except:
-            pass
+    # """ for a given entity, find properties with matching values in both languages."""
+
+        with open('data/{}/{}.csv'.format(src_lang, entity), encoding='utf-8') as f:
+            srcreader = csv.reader(f)
+            prop_matches = []
+            for src_row in srcreader:
+
+
+                try:
+                    with open('data/{}/{}.csv'.format(trg_lang, entity), encoding='utf-8') as g:
+                        trgreader = csv.reader(g)
+                        for trg_row in trgreader:
+
+                            if src_row[1] == trg_row[1]:
+                                prop_matches.append([src_row[0], trg_row[0], src_row[1]])
+                except:
+                    pass
+        print(entity, '\n', prop_matches)
 
 #find_entity_match('website', 'en', 'nl')
 find_matching_prop('!!!', 'en', 'nl')
